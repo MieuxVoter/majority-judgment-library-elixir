7f2a4cf5bda5e7c52e9319a27af339b3a2680ca2
domi41 <dominique.merle.41@gmail.com>
2021-10-14 21:08:46 +0200

    test: write a basic usage of the lib
    That's enough to start coding!
    /spend 1d

--- test/judgment_test.exs
+++ test/judgment_test.exs
@@ -2,7 +2,17 @@ defmodule JudgmentTest do
   use ExUnit.Case
   doctest Judgment
 
-  test "greets the world" do
-    assert Judgment.hello() == :world
+  test "Resolve Majority Judgment" do
+    tallies = [
+      [0, 1, 2, 3, 4],
+      [0, 2, 1, 3, 4],
+      [2, 1, 0, 3, 4],
+    ]
+    result = Judgment.Majority.resolve(tallies)
+    assert result != nil
+    assert result.proposals != nil
+    assert length(result.proposals) == length(tallies)
+    assert (for p <- result.proposals, do: p.index) == [0, 1, 2]
+    assert (for p <- result.proposals, do: p.rank) == [1, 2, 3]
   end
 end
