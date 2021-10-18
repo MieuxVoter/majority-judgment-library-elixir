c2abc1fafd44a886c2eab1c12bcce5da85261e60
domi41 <dominique.merle.41@gmail.com>
2021-10-18 07:27:25 +0200

    feat: add an option to favor contestation
    Not sure about the case, though.
    Gotta read up some more about elixir!

--- lib/judgment/majority.ex
+++ lib/judgment/majority.ex
@@ -17,36 +17,43 @@ defmodule Judgment.Majority do
       iex> assert (for p <- r.proposals, do: p.rank) == [1, 2]
 
   """
-  def resolve(pollTally) do
+  def resolve(pollTally, options \\ []) do
+    default = [
+      favorContestation: true,
+#      scoreWithSeparators: true,
+    ]
+    options = Keyword.merge(default, options)
 
     analyses = pollTally
-               |> Enum.map(fn proposalTally -> Judgment.Majority.Analysis.runOn(proposalTally) end)
+               |> Enum.map(
+                    fn proposalTally ->
+                      Judgment.Majority.Analysis.runOn(proposalTally, options[:favorContestation])
+                    end
+                  )
 
     scores = pollTally
              |> Enum.map(fn proposalTally -> computeScore(proposalTally) end)
              |> Enum.with_index()
-#             |> IO.inspect(label: "scores")
 
     sortedScores = scores
-                   # Doing two reverse() is probably not the most efficient way
+                   # Doing two reverse() is perhaps not the most efficient way
                    # We aim to keep the input order when there are equalities.
                    |> Enum.reverse()
-                   |> Enum.sort_by(fn {s, _i} -> s end)
+                   |> Enum.sort_by(fn {score, _index} -> score end)
                    |> Enum.reverse()
                    |> Enum.with_index()
-                   |> Enum.map(fn {{s, i}, is} -> {s, i, is} end)
-    #                   |> IO.inspect(label: "sorted scores")
+      #            |> Enum.map(fn t -> Tuple.flatten(t) end)  # Tuple.flatten/1 is undefined
+                   |> Enum.map(fn {{score, index}, sortedIndex} -> {score, index, sortedIndex} end)
 
     proposals = sortedScores
-                |> Enum.sort_by(fn {_s, i, _is} -> i end)
+                |> Enum.sort_by(fn {_score, index, _sortedIndex} -> index end)
                 |> Enum.map(
                      fn {score, index, sortedIndex} ->
                        amountOfEqualsAbove =
                          sortedScores
-                         |> Enum.take_while(fn {_s, i, _is} -> i != index end)
-                         |> Enum.filter(fn {s, _i, _is} -> s == score end)
+                         |> Enum.take_while(fn {_s, i, _si} -> i != index end)
+                         |> Enum.filter(fn {s, _i, _si} -> s == score end)
                          |> Enum.count
-                       #                         |> IO.inspect(label: "amountOfEqualsAbove")
                        rank = sortedIndex + 1 - amountOfEqualsAbove
                        %Judgment.Majority.ProposalResult {
                          rank: rank,
@@ -58,7 +65,7 @@ defmodule Judgment.Majority do
                    )
 
     sortedProposals = proposals
-                      # Double reverse again, please help me
+                      # Double reverse again, please have mercy
                       |> Enum.reverse()
                       |> Enum.sort_by(fn p -> p.score end)
                       |> Enum.reverse()
@@ -69,14 +76,18 @@ defmodule Judgment.Majority do
     }
   end
 
-  defp computeScore(proposalTally) do
-    computeScore(proposalTally, Enum.count(proposalTally))
+  defp computeScore(proposalTally, options \\ []) do
+    default = [
+      inner_sep: '_',
+      outer_sep: '/',
+    ]
+    options = Keyword.merge(default, options)
+    computeScore(proposalTally, options, Enum.count(proposalTally))
   end
 
-  defp computeScore(proposalTally, depth) do
-    #    IO.inspect(proposalTally, label: "computeScore")
-    inner_sep = '_'
-    outer_sep = '/'
+  defp computeScore(proposalTally, options, depth) do
+    inner_sep = options[:inner_sepe] || '_'
+    outer_sep = options[:outer_sep] || '/'
     analysis = Judgment.Majority.Analysis.runOn(proposalTally)
     amountOfGrades = length(proposalTally)
     amountOfDigitsForGrade = computeAmountOfDigits(amountOfGrades)
@@ -97,6 +108,7 @@ defmodule Judgment.Majority do
       computeScore(
         proposalTally
         |> regradeJudgments(analysis.medianGrade, analysis.secondGroupGrade),
+        options,
         depth - 1
       )
     else
@@ -124,7 +136,6 @@ defmodule Judgment.Majority do
              end
            end
          )
-      #      |> IO.inspect(label: "regraded")
     end
   end
 
